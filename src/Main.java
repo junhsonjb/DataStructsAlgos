103e11dd491300f906c0dbaa519c4ee37811356a
Junhson <jjeanba2@binghamton.edu>
2018-01-05 03:17:37 -0500

    just added a comment to an unused file for clarity

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        /*
+        IntelliJ just made this file, but as of yet,
+        I don't use it, so it's just here.
+         */
         System.out.println("HIII");
     }
 }
