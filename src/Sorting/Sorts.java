bdccc3fe272e76494c370413f0acc7fc6a6134a0
Junhson <jjeanba2@binghamton.edu>
2018-01-07 00:32:42 -0500

    added explanation for each sorting method

--- src/Sorting/Sorts.java
+++ src/Sorting/Sorts.java
@@ -5,7 +5,13 @@ import java.util.Arrays;
 
 public class Sorts {
 
-
+    /* Eplanation:
+    A selection sort looks at the entire array, picks the minimum and swaps it with the 0th
+    element. Then it picks the next minimum (the least value after the previous minimum)
+    and swaps it with the 1st element, since everything before it is sorted. It repeats this
+    process up to the ith element where i is equal to number of elements in the array.
+    Run Time: O(N^2)
+     */
     public static void selection(int[] a) {
 
         //we're gonna want to do this for each element in the array
@@ -30,8 +36,15 @@ public class Sorts {
 
     }
 
-    /* WORKS, thank God */
-    /* There is an implementation of this method that uses booleans and a while/do While loop. I prefer this way */
+    /* Explanation:
+    The bubble sort compares the 0th and 1st elements and if the 0th is greater than
+     the 1st swaps them to get an increasing order. Then it does this with 1st and 2nd,
+     and so on until it compares the ith and i+1th values, where i is equal to the
+     length of the arrays minus 1 (to account for zero indexing). We do this process the
+     amount of times equalvalent to the amount of elements in the array, but with each
+     iteration, we reduce i by one. This way we are not repeating the process on elemeents
+     that are already sorted (elements are sorted starting from the end going to the front).
+     */
     public static void bubble(int[] a) {
 
         int temp;
@@ -58,7 +71,7 @@ public class Sorts {
 
     public static void main(String[] args) {
 
-        int[] a = {12, 1, 8, 7, 6};
+        int[] a = {12, 1, 8, 7, 6, -1};
         //selection(a);
         bubble(a);
         System.out.println(Arrays.toString(a));
